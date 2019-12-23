492c841d945fb0b20e128485f5da32a2d76eee47
Brookton <davidbrookton@gmail.com>
2019-12-23 16:16:44 -0700

    Changed Product Link

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import './App.css';
 
 class App extends React.Component {
     redirect(){
-      window.location.href="https://bluebirdbotanicals.com";
+      window.location.href="https://bluebirdbotanicals.com/product-category/topicals/";
     }
     render(){
     return (
